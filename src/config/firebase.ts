144c8ca443927a8763922269cd03b51f0661425e
Brad Goodall <bradgoodall94@gmail.com>
2023-06-10 13:29:02 +1000

    Removed unused dependencies

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,6 +1,6 @@
 
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+//import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider } from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
 
@@ -16,7 +16,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+//const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider;
 export const db = getFirestore(app);
